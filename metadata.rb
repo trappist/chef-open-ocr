26555d9a256de9c88ba09c24e9dc2cf6fe267151
Rocco Stanzione <grasshopper@linuxkungfu.org>
2015-03-16 16:05:40 -0500

    First commit

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'open-ocr'
+maintainer       'Rocco Stanzione'
+maintainer_email 'trappist@gmail.com'
+license          'Apache 2.0'
+description      'Installs tleyden/open-ocr'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.2'
+
+depends          'build-essential'
+depends          'golang'
+depends          'tesseract'
---
+++ recipes/default.rb
@@ -0,0 +1,37 @@
+#
+# Cookbook Name:: open-ocr
+# Recipe:: default
+#
+# Copyright 2015, Rocco Stanzione
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+ENV['GOPATH'] = '/opt/go'
+
+directory ENV['GOPATH'] do
+  action :create
+end
+
+bash "build_open_ocr" do
+  cwd ENV['GOPATH']
+  code <<-EOH
+    go get -u -v -t github.com/tleyden/open-ocr
+    cd $GOPATH/src/github.com/tleyden/open-ocr/cli-httpd
+    go build -v -o open-ocr-httpd
+    cp open-ocr-httpd /usr/bin
+    cd $GOPATH/src/github.com/tleyden/open-ocr/cli-worker
+    go build -v -o open-ocr-worker
+    cp open-ocr-worker /usr/bin
+  EOH
+end

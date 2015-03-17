fb8a05e17549486925f55ea61bca6cdf7c385e91
Rocco Stanzione <grasshopper@linuxkungfu.org>
2015-03-17 13:59:11 -0500

    Start open-ocr worker and webservice

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,7 +24,6 @@
 end
 
 gopath = '/opt/go'
-ENV['GOPATH'] = gopath
 
 directory gopath do
   action :create
@@ -40,5 +39,14 @@ bash "build_open_ocr" do
     cd #{gopath}/src/github.com/tleyden/open-ocr/cli-worker
     go build -v -o open-ocr-worker
     cp open-ocr-worker /usr/bin
+  EOH
+end
+
+bash "start_open_ocr" do
+  code <<-EOH
+    export HTTP_PORT=8080
+    export AMQP_URI="amqp://guest:guest@localhost/"
+    open-ocr-worker -amqp_uri "${AMQP_URI}"
+    open-ocr-httpd -amqp_uri "${AMQP_URI}" -http_port ${HTTP_PORT}
   EOH
 end
